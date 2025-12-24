29db7d01eac44604a044d6396799c1e477ac8d0c
Ahmet Aydin <ahaydin@ebay.com>
2025-12-24 15:34:00 +0100

    Some global config changes and Claude Code support

--- lua/configs/configs.lua
+++ lua/configs/configs.lua
@@ -6,6 +6,7 @@ vim.opt.relativenumber = true
 vim.opt.expandtab = true
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
 
 -- Nvim Tree Configs
 vim.g.loaded_netrw = 1
@@ -13,3 +14,11 @@ vim.g.loaded_netrwPlugin = 1
 
 --- Enable 24-bit colors (Optional)
 vim.opt.termguicolors = true
+
+-- Disable Status Line Since We Are Using Lualine
+vim.opt.laststatus = 0
+vim.opt.showmode = false
+
+-- Enable Mouse Support
+vim.opt.mouse = 'a'
+
--- lua/configs/keymaps.lua
+++ lua/configs/keymaps.lua
@@ -1,2 +1,9 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
+
+-- For Claude Code
+vim.keymap.set("n", "<leader>ai", function()
+	vim.cmd("botright split")
+	vim.cmd("resize 20")
+	vim.cmd("terminal claude")
+end, { desc = "Open: [C][l]aude [C]ode" })
